178cfdb26eaacd7e307b5743b1f461399695eb6d
Martin Leissler <martin@leissler.net>
2015-06-15 09:06:17 +0200

    Added a Projectile Trail Upgraded project to Unity 5.1

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -2,18 +2,28 @@
 using System.Collections;
 
 public class Goal : MonoBehaviour {
-
-	// A static field visible from anywhere
+	// Static field accessible from anywhere
+	// Storing if the goal was met
 	public static bool goalMet = false;
 
+
+	// Trigger got entered
 	void OnTriggerEnter(Collider other) {
-		// Check if the hit comes from a projectile
+		// Check if the object entering the trigger is a projectile
 		if(other.gameObject.tag == "Projectile") {
+			// Set the staic field to true
 			goalMet = true;
-			// Set alpha to higher opacity
-			Color c = GetComponent<Renderer>().material.color;
-			c.a = 1.0f;
-			GetComponent<Renderer>().material.color = c;
+			// Set the alpha of the color to a higher opacity
+
+			Color c = this.gameObject.GetComponent<Renderer>().material.color;
+
+			c.a = 1;
+
+			this.gameObject.GetComponent<Renderer>().material.color = c;
+
 		}
+	
 	}
+
+
 }
